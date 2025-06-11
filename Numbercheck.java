1f5c330c566eb12fda7b0b95ee05ce2141272a93
abhi0256 <pavitrapandith595@gmail.com>
2025-06-11 12:35:09 +0530

    checked positive number

---
+++ Numbercheck.java
@@ -0,0 +1,14 @@
+public class Numbercheck{
+    public static void main(string[] args){
+        int num=-5;
+        if(num>0){
+            System.out.println("the number is positive");
+
+        }else if(num<0){
+            System.out.println("the number is negative");
+
+        }else{
+            System.out.println("the number is zero");
+        }
+    }
+}
